d514a1ca06a87e9b05ed5c1056279a688cbc2ec1
prakharsaxena007 <114650147+prakharsaxena007@users.noreply.github.com>
2022-10-09 00:19:14 +0530

    Create Badger.java

---
+++ Badger.java
@@ -0,0 +1,12 @@
+public class Badger{
+    public static main void(Strings [] args){
+      int a =10, b=12;
+      
+      if( a > b){
+        System.out.print("A is grater than B");
+      }
+      else{
+        System.out.print("B is greater than A")
+    }
+
+}
